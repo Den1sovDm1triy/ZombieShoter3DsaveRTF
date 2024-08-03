fe3c93a1c375bf2a1e64361d161f01c281c9f7d5
Den1sovDm1triy <mr.denisov.dv@gmail.com>
2024-08-03 21:14:23 +0500

    upgrade buttons

--- Assets/Scripts/Money/MoneyItem.cs
+++ Assets/Scripts/Money/MoneyItem.cs
@@ -8,6 +8,8 @@ public class MoneyItem : MonoBehaviour, IItem
     public static Action<Vector3, int> onTakeMoney;
     [SerializeField] private int amount;
     [SerializeField] private Outline outline;
+    [SerializeField] private float flightDuration = 3.0f; // Продолжительность полета
+    [SerializeField] private float flightHeight = 1.0f; // Высота траектории полета
     bool first;
     public void DropItem()
     {
@@ -42,8 +44,31 @@ public class MoneyItem : MonoBehaviour, IItem
     void Start()
     {
           transform.DORotate(new Vector3(0, 360, 0), 1.0f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Incremental);
+          MoveToPlayer();
     }
 
-    
-    
+    void MoveToPlayer()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            Vector3 playerPosition = player.transform.position;
+            Vector3 startPosition = transform.position;
+            Vector3 midPoint = startPosition + (playerPosition - startPosition) / 2 + Vector3.up * flightHeight;
+
+            Vector3[] path = new Vector3[] { midPoint, playerPosition };
+
+            transform.DOPath(path, flightDuration, PathType.CatmullRom)
+                .SetEase(Ease.InOutSine)
+                .OnComplete(() =>
+                {
+                    onTakeMoney?.Invoke(transform.position, amount);
+                    Destroy(this.gameObject);
+                });
+        }
+        else
+        {
+            Debug.LogWarning("Player object not found.");
+        }
+    }
 }
